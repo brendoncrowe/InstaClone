917ee3737a8f6624004d0d9ab3877412a1ea6ce8
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-04-30 16:32:01 -0400

    continuing development on camera

--- InstaClone/Controllers/CameraController.swift
+++ InstaClone/Controllers/CameraController.swift
@@ -20,17 +20,22 @@ class CameraController: UIViewController {
         return button
     }()
     
+    let backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "arrowshape.right.fill")?.withTintColor(.label, renderingMode: .alwaysOriginal), for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-
         setupCaptureSession()
         setupConstraints()
         capturePhotoButton.addTarget(self, action: #selector(handleCapturePhoto), for: .touchUpInside)
         navigationController?.navigationBar.isHidden = true
     }
     
-
     private func setupCaptureSession() {
         let captureSession = AVCaptureSession()
         guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
@@ -44,14 +49,13 @@ class CameraController: UIViewController {
         } catch {
             print(error)
         }
-        
         if captureSession.canAddOutput(outPut) {
             captureSession.addOutput(outPut)
         }
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resize
         view.layer.addSublayer(previewLayer)
         previewLayer.frame = view.frame
-        previewLayer.videoGravity = .resizeAspectFill
         DispatchQueue.global(qos: .background).async {
             captureSession.startRunning()
         }
@@ -59,22 +63,35 @@ class CameraController: UIViewController {
     
     private func setupConstraints() {
         view.addSubview(capturePhotoButton)
+        view.addSubview(backButton)
         NSLayoutConstraint.activate([
             capturePhotoButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -60),
             capturePhotoButton.widthAnchor.constraint(equalToConstant: 80),
             capturePhotoButton.heightAnchor.constraint(equalTo: capturePhotoButton.widthAnchor),
             capturePhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+        
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 64),
+            backButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -18)
+        ])
     }
     
     @objc private func handleCapturePhoto() {
-        print("capturing photo")
+        let settings = AVCapturePhotoSettings()
+        guard let previewFormatType = settings.availablePreviewPhotoPixelFormatTypes.first else { return }
+        settings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewFormatType]
+        outPut.capturePhoto(with: settings, delegate: self)
     }
 }
 
 extension CameraController: AVCapturePhotoCaptureDelegate {
     
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        let previewImage = UIImage(data: imageData)
+        let photoPreviewImageView = UIImageView(image: previewImage)
+        photoPreviewImageView.frame = view.frame
+        self.view.addSubview(photoPreviewImageView)
     }
 }
